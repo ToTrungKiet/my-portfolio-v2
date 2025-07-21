1b6b03cd1d2f035f79a61ac379274746c0e27e6c
totrungkiet26102003 <totrungkiet26102003@example.com>
2025-07-21 12:18:00 +0700

    Build footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,10 @@ import { AboutSection } from "@/components/AboutSection";
 import { SkillsSection } from "@/components/SkillsSection";
 import { ProjectsSection } from "@/components/ProjectsSection";
 import { ContactSection } from "@/components/ContactSection";
+import { Footer } from "@/components/Footer";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      
       {/* Theme toggle */}
         <ThemeToggle />
       {/* Background Effects */}
@@ -25,6 +25,7 @@ export const Home = () => {
           <ContactSection />
         </main>
       {/* Footer */}
+        <Footer />
     </div>
   );
 }
